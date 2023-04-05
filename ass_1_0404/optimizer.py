f6577a8524ccde5c038781b566ab9ee3d2d562f4
yezhipeng2417 <yezhipeng99@foxmail.com>
2023-04-05 23:33:09 +1000

    graphing, test, dev

--- ass_1_0404/optimizer.py
+++ ass_1_0404/optimizer.py
@@ -36,7 +36,7 @@ class Adam(object):
         self.lr = lr
         self.decay_rate = 1.0 - decay
         self.eps = eps
-        self.parameters = [p for p in parameters if p.requires_grad]
+        self.parameters = parameters
         self.accumulated_grad_mom = [np.zeros(p.data.shape) for p in self.parameters]
         self.accumulated_grad_rms = [np.zeros(p.data.shape) for p in self.parameters]
 
@@ -45,7 +45,7 @@ class Adam(object):
         self.accumulated_beta2 *= self.beta2
         lr = self.lr * ((1 - self.accumulated_beta2)**0.5) / (1 - self.accumulated_beta1)
         for p, grad_mom, grad_rms in zip(self.parameters, self.accumulated_grad_mom, self.accumulated_grad_rms):
-            if self.decay_rate < 1 and not p.skip_decay: p.data *= self.decay_rate
+            p.data *= self.decay_rate
             np.copyto(grad_mom, self.beta1 * grad_mom + (1 - self.beta1) * p.grad)
             np.copyto(grad_rms, self.beta2 * grad_rms + (1 - self.beta2) * np.power(p.grad, 2))
             p.data -= lr * grad_mom / (np.sqrt(grad_rms) + self.eps)
