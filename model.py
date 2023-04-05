ed9aa47b46aeae0f036d79f3e78e5d3abc943790
yezhipeng2417 <yezhipeng99@foxmail.com>
2023-04-06 04:01:39 +1000

    manage

---
+++ model.py
@@ -0,0 +1,75 @@
+import numpy as np
+from linear import Linear
+from activations import *
+from batchnorm import BatchNorm
+from dropout import Dropout
+from inputnorm import InputNorm
+
+class Model:
+    def __init__(self, layer_configs):
+        self.layers = []
+        self.params = []
+        self.dropout_ix = []
+        self.norm_ix=[]
+        for config in layer_configs:
+            try:
+                hyperparam = config.get('hyperparam', None)
+                if hyperparam is not None:
+                    layer = eval(f"{config['name']}(**hyperparam)")
+                else:
+                    layer = eval(f"{config['name']}()")
+                self.layers.append(layer)
+                if config['name'] == 'Dropout':
+                    self.dropout_ix.append(1)
+                else:
+                    self.dropout_ix.append(0)
+
+
+                if config['name'] == 'InputNorm':
+                    self.norm_ix.append(1)
+                else:
+                    self.norm_ix.append(0)
+
+                if hasattr(layer, 'W'): self.params.append(layer.W)
+                if hasattr(layer, 'b'): self.params.append(layer.b)
+            except Exception as e:
+                print(e)
+                continue
+        if sum(self.dropout_ix) == 0:
+            self.dropout_ix = -1
+        else:
+            self.dropout_ix = np.argmax(np.array(self.dropout_ix))
+
+        if sum(self.norm_ix) == 0:
+            self.norm_ix = -1
+        else:
+            self.norm_ix = np.argmax(np.array(self.norm_ix))
+        
+    
+    def train_mode(self):
+        if self.dropout_ix != -1:
+            self.layers[self.dropout_ix].is_test = False
+            
+        if self.norm_ix != -1:
+            self.layers[self.norm_ix].is_test = False
+    
+    def test_mode(self):
+        if self.dropout_ix != -1:
+            self.layers[self.dropout_ix].is_test = True
+
+        if self.norm_ix != -1:
+            self.layers[self.norm_ix].is_test = True
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
+    
+    def backward(self, eta):
+        for layer in self.layers[::-1]:
+            eta = layer.backward(eta)
+        return eta
+            
+
+
+    
