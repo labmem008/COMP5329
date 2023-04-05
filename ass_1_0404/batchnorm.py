868d0082240407dd29fbfd60fda41db74768d5a4
Hanming <arcanemastery@163.com>
2023-04-06 02:03:18 +1000

    module txt

--- ass_1_0404/batchnorm.py
+++ ass_1_0404/batchnorm.py
@@ -10,11 +10,11 @@ class BatchNorm:
         self.overall_var = Parameter(np.zeros(shape))
         self.overall_ave = Parameter(np.zeros(shape))
     def forward(self, x):
-        if self.is_test:
-            # 进行测试时使用估计的训练集的整体方差和均值进行归一化
-            sample_ave = self.overall_ave.data
-            sample_std = np.sqrt(self.overall_var.data)
-        else:
+        if not self.is_test:
+        #     # 进行测试时使用估计的训练集的整体方差和均值进行归一化
+        #     sample_ave = self.overall_ave.data
+        #     sample_std = np.sqrt(self.overall_var.data)
+        # else:
             # 进行训练时使用样本的均值和方差对训练集整体的均值和方差进行估计（使用加权平均的方法）
             sample_ave = x.mean(axis=0)
             sample_var = x.var(axis=0)
