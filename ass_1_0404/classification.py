96242d7193f21d943664ee7be2d399c892760a85
yezhipeng2417 <yezhipeng99@foxmail.com>
2023-04-05 19:57:19 +1000

    test, dev

--- ass_1_0404/classification.py
+++ ass_1_0404/classification.py
@@ -4,9 +4,14 @@ import numpy as np
 from tqdm import tqdm 
 from optimizer import *
 from model import Model
+from activations import Softmax
 from cross_entropy import CrossEntropy
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
 
+
+softmax = Softmax()
 
 def save(parameters, save_as):
     dic = {}
@@ -30,18 +35,21 @@ def load_data(train_data_path, train_label_path, transform=False):
     return X, Y
 
 
-def train(net, loss_fn, train_data_path, train_label_path, batch_size, optimizer, load_file, save_as, times=1, retrain=False):
+def train(net, loss_fn, train_data_path, train_label_path, test_data_path, test_label_path, batch_size, optimizer, load_file, save_as, epoches=1, patience=5):
+    best_loss = 1e3
+    best_model = net
     X, Y = load_data(train_data_path, train_label_path, transform=True)
-    data_size = X.shape[0]
-    if not retrain and os.path.isfile(load_file): load(net.parameters, load_file)
-    with tqdm(total=times, desc=f'Epoch0') as pbar:
-        for loop in range(times):
+    test_X, test_Y = load_data(test_data_path, test_label_path, transform=True)
+    train_X, dev_X, train_Y, dev_Y = train_test_split(X, Y, test_size=0.15, random_state=42)
+    data_size = train_X.shape[0]
+    with tqdm(total=epoches, desc=f'Epoch0') as pbar:
+        for epoch in range(epoches):
             i = 0
             batch_losses = []
             batch_acces = []
             while i <= data_size - batch_size:
-                x = X[i:i+batch_size]
-                y = Y[i:i+batch_size]
+                x = train_X[i:i+batch_size]
+                y = train_Y[i:i+batch_size]
                 i += batch_size
 
                 output = net.forward(x)
@@ -54,47 +62,75 @@ def train(net, loss_fn, train_data_path, train_label_path, batch_size, optimizer
                 batch_acces.extend(batch_acc)
             epoch_loss = np.average(batch_losses)
             epoch_acc = np.average(batch_acces)
-            pbar.set_description(f'Epoch{loop}, epoch_loss: {round(epoch_loss, 4)}, acc: {round(epoch_acc, 4)*100}%')
+            dev_acc, dev_loss, dev_rpt = test(net, loss_fn, dev_X, dev_Y)
+            dev_acc = np.average(dev_acc)
+            pbar.set_description(f'Epoch{epoch}, epoch_loss: {round(epoch_loss, 4)}, acc: {round(epoch_acc, 4)*100}%, dev_loss: {round(dev_loss, 4)}, dev_acc: {round(dev_acc, 4)*100}%')
             pbar.update(1)
+            # if epoch%5==0:
+            #     print('dev report', dev_rpt, sep='\n')
+            # early_stopping
+            if dev_loss < best_loss:
+                best_loss = min(dev_loss, best_loss)
+                best_model = net
+                p = 0
+            else:
+                p += 1
+            if p > patience:
+                break
+        test_acc, test_loss, test_rpt = test(best_model, loss_fn, test_X, test_Y)
+        test_acc = np.average(test_acc)
+        print(f'epoch_loss: {round(test_loss, 4)}, acc: {round(test_acc, 4)*100}%\n{test_rpt}')
         if save_as is not None: save(net.parameters, save_as)
     
+def test(net, loss_fn, data, label):
+    output = net.forward(data)
+    prediction = softmax.forward(output)
+    acc, loss = loss_fn(output, label)
+    rpt = classification_report(np.argmax(label, axis=1), np.argmax(prediction, axis=1))
+    return acc, loss, rpt
+
+
+
 
 if __name__ == "__main__": 
     layers = [
         {'name': 'InputNorm', 'hyperparam': {'shape': 128}},
 
-        {'name': 'Linear', 'hyperparam': {'in_dim': 128, 'out_dim': 256}},
-        {'name': 'BatchNorm', 'hyperparam': {'shape': 256}},
-        {'name': 'Gelu'},
-        {'name': 'Dropout', 'hyperparam': {'drop_rate': 0.4}},
-
-        {'name': 'Linear', 'hyperparam': {'in_dim': 256, 'out_dim': 512}},
-        {'name': 'BatchNorm', 'hyperparam': {'shape': 512}},
-        {'name': 'Gelu'},
-        # {'name': 'Dropout', 'hyperparam': {'drop_rate': 0.2}},
-
-        {'name': 'Linear', 'hyperparam': {'in_dim': 512, 'out_dim': 256}},
-        {'name': 'BatchNorm', 'hyperparam': {'shape': 256}},
-        {'name': 'Gelu'},
-        # {'name': 'Dropout', 'hyperparam': {'drop_rate': 0.2}},
-
-        {'name': 'Linear', 'hyperparam': {'in_dim': 256, 'out_dim': 128}},
+        {'name': 'Linear', 'hyperparam': {'in_dim': 128, 'out_dim': 128}},
         {'name': 'BatchNorm', 'hyperparam': {'shape': 128}},
-        {'name': 'Gelu'},
-        # {'name': 'Dropout', 'hyperparam': {'drop_rate': 0.2}},
+        {'name': 'Relu'},
+
+        # {'name': 'Linear', 'hyperparam': {'in_dim': 128, 'out_dim': 256}},
+        # {'name': 'BatchNorm', 'hyperparam': {'shape': 256}},
+        # {'name': 'Relu'},
+
+        # {'name': 'Linear', 'hyperparam': {'in_dim': 256, 'out_dim': 512}},
+        # {'name': 'BatchNorm', 'hyperparam': {'shape': 512}},
+        # {'name': 'Relu'},
+
+        # {'name': 'Linear', 'hyperparam': {'in_dim': 512, 'out_dim': 256}},
+        # {'name': 'BatchNorm', 'hyperparam': {'shape': 256}},
+        # {'name': 'Relu'},
+
+        # {'name': 'Linear', 'hyperparam': {'in_dim': 256, 'out_dim': 128}},
+        # {'name': 'BatchNorm', 'hyperparam': {'shape': 128}},
+        # {'name': 'Relu'},
 
         {'name': 'Linear', 'hyperparam': {'in_dim': 128, 'out_dim': 16}},
         {'name': 'BatchNorm', 'hyperparam': {'shape': 16}},
-        {'name': 'Gelu'},
-        # {'name': 'Dropout', 'hyperparam': {'drop_rate': 0.2}},
+        {'name': 'Relu'},
+        # {'name': 'Dropout', 'hyperparam': {'drop_rate': 0.3}},
 
         {'name': 'Linear', 'hyperparam': {'in_dim': 16, 'out_dim': 10}},
     ]
     loss_fn = CrossEntropy()
     net = Model(layers)
     lr = 0.1
-    batch_size = 512
+    batch_size = 128
     optimizer = SGD(net.params, lr, decay=0)
     train_data_path = './data/train_data.npy'
     train_label_path = './data/train_label.npy'
-    train(net, loss_fn, train_data_path, train_label_path, batch_size, optimizer, None, None, times=50, retrain=True)
+    test_data_path = './data/test_data.npy'
+    test_label_path = './data/test_label.npy'
+    data_files = (train_data_path, train_label_path, test_data_path, test_label_path)
+    train(net, loss_fn, *data_files, batch_size, optimizer, None, None, epoches=100, patience=10)
